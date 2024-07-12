fefb5a0ed9fdb438c201a60c289ee280f82d947e
cris-cmk <muthuikibara@gmail.com>
2024-07-12 03:05:07 -0500

    added read me file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # functional-programming
-Practice writing pure functions, closures, curried functions, and recursive functions.
+Practice project for writing pure functions, closures, curried functions, and recursive functions.
+This has been implemented using functional programming and webpack
+
+To run the  project 
+1.  git clone https://github.com/cris-cmk/functional-programming
+2.  cd functional-programming
+3.  npm install
+4.  npx serve
+5.  npm test
+
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { toRoman } from './romanNumerals';
 import { sieveOfEratosthenes } from './primeSieve';
 
 //sample use case
-console.log(sieveOfEratosthenes(30)); // [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-console.log(coinCounterRecursive(499)); // [19, 2, 0, 4]
+console.log(sieveOfEratosthenes(30)); 
+console.log(coinCounterRecursive(499)); 
 const countCoins = createCoinCounter();
-console.log(countCoins(499)); // [19, 2, 0, 4]
-console.log(toRoman(3999)); // MMMCMXCIX
+console.log(countCoins(499));
+console.log(toRoman(3999)); 
